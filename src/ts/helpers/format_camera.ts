3442df279122e60575c9088acf8b0be89cec4ea7
Xavier Shay <contact@xaviershay.com>
2025-11-04 10:26:39 +1100

    missed a character

--- src/ts/helpers/format_camera.ts
+++ src/ts/helpers/format_camera.ts
@@ -1,6 +1,6 @@
 export const formatCamera = (raw : string): string => {
     switch (raw) {
-        case "COOLPIX 950": return "Nikon COOLPIX 950";
+        case "COOLPIX P950": return "Nikon COOLPIX 950";
         case "OM-1MarkII": return "OM-1 Mark II";
         default: return raw
     }
